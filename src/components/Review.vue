b085c75b306c8740189d3a0b20eb6810214421f1
Philippe De Ryck <philippe@pragmaticwebsecurity.com>
2025-01-26 12:16:06 +0100

    Added Review component

---
+++ src/components/Review.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+import {onMounted, useTemplateRef} from "vue";
+
+const props = defineProps({
+  review: Object
+});
+
+
+// Add the review content to the page
+const content = useTemplateRef('content')
+onMounted(() => {
+  content.value.innerHTML = props.review.content;
+})
+
+</script>
+
+<template>
+  <div class="card-body">
+    <h4 class="card-title">
+      <span class="font-weight-bold">{{ props.review.rating }}/10</span>
+      <span class="font-weight-light ml-3">{{ props.review.title }}</span>
+      <span class="float-end">
+            <router-link
+                class="p-2 small btn btn-primary align-self-end"
+                :to="`/reviews/${props.review.id}/edit`"
+            >
+              Edit
+            </router-link>
+          </span>
+    </h4>
+    <p
+        ref="content"
+        class="card-text pt-2"
+    />
+  </div>
+</template>
--- src/views/RestaurantReviews.vue
+++ src/views/RestaurantReviews.vue
@@ -1,10 +1,12 @@
 <script setup>
 import DataService from "@/data/data.service";
 import {useRoute} from "vue-router";
+import Review from "@/components/Review.vue";
 
 const route = useRoute();
 const restaurant = DataService.getRestaurantById(route.params.id);
 const reviews = DataService.getReviewsByRestaurantId(route.params.id);
+
 </script>
 
 <template>
@@ -18,24 +20,7 @@ const reviews = DataService.getReviewsByRestaurantId(route.params.id);
         :key="review.id"
         class="card w-75 mt-4 mb-2"
     >
-      <div class="card-body">
-        <h4 class="card-title">
-          <span class="font-weight-bold">{{ review.rating }}/10</span>
-          <span class="font-weight-light ml-3">{{ review.title }}</span>
-          <span class="float-end">
-            <router-link
-                class="p-2 small btn btn-primary align-self-end"
-                :to="`/reviews/${review.id}/edit`"
-            >
-              Edit
-            </router-link>
-          </span>
-        </h4>
-        <p
-            class="card-text pt-2"
-            v-html="review.content"
-        />
-      </div>
+      <Review :review="review"></Review>
     </div>
   </div>
 </template>
